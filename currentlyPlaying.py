19a938b573bc423e72f7dfb8e7d01c11233165fa
zjones2142 <zachjones2142@gmail.com>
2024-09-25 17:38:46 -0500

    zj - compiling script into executable + adding error handling

--- currentlyPlaying.py
+++ currentlyPlaying.py
@@ -2,6 +2,8 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import ctypes
+import requests
+import time
 
 # Replace with your Spotify Client ID and Client Secret
 client_id = '0cd9be3e63aa405da833af8595b39b6f'
@@ -9,6 +11,9 @@ client_secret = '6b46a949153c4d73b705adad6e087e45'
 redirect_uri = 'http://localhost:8000'  # Replace with your actual redirect URI
 scope = 'user-read-currently-playing'
 mydict = {ord(x): '' for x in [":", ",", "!", ".", ";", "'", " ", "-"]}
+prevAlbumSaveFile = os.getcwd()+'\\previousSetBackground.txt'
+cwd = os.getcwd()
+albumDir = cwd+'\\AlbumArt\\'
 
 # Create a SpotifyOAuth object without automatic handling
 auth_manager = SpotifyOAuth(client_id=client_id, client_secret=client_secret,
@@ -27,28 +32,54 @@ def get_current_track_info():
         if current_track:
             artist_name = current_track['item']['artists'][0]['name']
             album_name = current_track['item']['album']['name']
-            return artist_name, album_name
+            album_art = current_track['item']['album']['images'][0]['url']
+            return artist_name, album_name, album_art
         else:
             print("Nothing Was Playing...")
-            return None, None
+            return None, None, None
     except Exception as e:
         print(f"Error fetching track info: {e}")
-        return None, None
+        return None, None, None
 
-def setBackgrnd(album):
-    parentDir = 'C:\\Users\\zachj\\Desktop\\PythonShtuff\\AlbumArtToPFP\\AlbumArt'
-    dirList = os.listdir(parentDir)
-    wallPaperStyle = 0
-    SPI_SETDESKWALLPAPER = 20
+def getAlbumArt(album, url):
+    if (url != None):
+        res = requests.get(url)
+
+        with open(albumDir+album+'.jfif', 'wb') as f:
+            print("Album art not found, downloading...")
+            f.write(res.content)
+            time.sleep(3)
+            if(album+'.jfif' in os.listdir(albumDir)):
+                print("Download complete.\nSetting wallpaper...")
+                return 0
+            else:
+                return 1
+    else:
+        return 2
+
+def setBackgrnd(album, art_url=None):
+    dirList = os.listdir(albumDir)
     file = 0
     if album+'.jfif' in dirList:
-        file = parentDir+'\\'+album+'.jfif'
+        file = cwd+'\\'+album+'.jfif'
+    else:
+        eCode = getAlbumArt(album, art_url)
+        if(eCode == 1):
+            print("Download failed.\nWaiting and retrying...\n")
+            time.sleep(2)
+            eCode = getAlbumArt(album, art_url)
+            setBackgrnd(album, art_url)
+        elif(eCode == 2):
+            print("Album url was missing.\nIssue with Spotify API. Check connection.\n")
+            return
+        else:
+            setBackgrnd(album, art_url)
     
     if file:
         image = ctypes.c_wchar_p(file)
-    
         try:
-            ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image, wallPaperStyle)
+            #args info: (SysParam to change, 0, image file, wallPaperStyle)
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, image, 0)
             print(f"Wallpaper set to {album}")
         except Exception as e:
             print(f"Error setting wallpaper: {e}")
@@ -56,23 +87,21 @@ def setBackgrnd(album):
 
 def checkGizz():
     #get data
-    try:
-        artist, album = get_current_track_info()
-    except Exception as e:
-        return 0
+    artist, album, art_url = get_current_track_info()
     
-    if(album != None):
+    if album:
         album = album.translate(mydict)
     else:
+        print("\n")
         artist = ''
 
-    with open('C:\\Users\\zachj\\Desktop\\PythonShtuff\\AlbumArtToPFP\\previousSetBackground.txt', 'r') as f:
+    with open(prevAlbumSaveFile, 'r') as f:
         previous_album = f.read().strip()
 
         if (artist == 'King Gizzard & The Lizard Wizard'):
             # Set the background and save the current album
-            setBackgrnd(album)
-            with open('previousSetBackground.txt', 'w') as f:
+            setBackgrnd(album, art_url)
+            with open(prevAlbumSaveFile, 'w') as f:
                 f.write(album)
         else:
             # Use the previous album
